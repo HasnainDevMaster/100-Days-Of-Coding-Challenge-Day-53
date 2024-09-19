feeea0f2fbe15e14c9b2338fffc40c535ef9492a
Syed Hasnain Ali Shah <syedhasnainali927@gmail.com>
2024-09-20 01:15:16 +0500

    Push

---
+++ Task.ts
@@ -0,0 +1,30 @@
+// 100 Days Of Coding Challenge!
+
+/** Day-53 Task:
+Watch the following video to learn about Chrome DevTools: Fun & powerful: Intro to Chrome DevTools*/
+
+//---------------------------------------------------------------------------------------------------
+
+/** 🚀 Getting Started with Chrome DevTools 🚀
+
+Just watched an insightful video by Jecelyn on the basics of Chrome DevTools! 🌟
+Whether you’re a beginner or looking to sharpen your skills,
+this tutorial is packed with essential tips and tricks.
+
+🔍 Key Highlights:
+
+Inspect Node: View and manipulate DOM elements.
+Elements Panel: Edit CSS directly.
+Device Mode: Simulate mobile views for responsive design.
+Console Panel: View logs, errors, and warnings.
+Sources Panel: Set breakpoints and debug your code.
+Network Panel: Filter network logs and throttle network speed.
+
+💡 Pro Tips:
+
+Sync edits with your workspace.
+Use console shortcuts for quicker commands.
+Select multiple network request types.
+Customize DevTools to fit your workflow.
+
+Check out the full video here: https://www.youtube.com/watch?v=t1c5tNPpXjs&t=23s */
